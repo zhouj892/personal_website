9bf1a7aba5152b2b2d5e8cbfac44c2057f4c78db
zhou Jiahui <zhou_jiahui_ca@hotmail.com>
2019-06-21 23:01:30 -0400

    my first change!

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hi! My name is Lina Zhou :D
         </a>
       </header>
     </div>
